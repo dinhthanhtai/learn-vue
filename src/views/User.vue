2b8b5842aff8f74fd03bb2a7099776d6bcda3e0d
GwynDinh <dinhthanhtai.xz@gmail.com>
2024-05-13 01:19:50 +0700

    project-learn

---
+++ src/views/User.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="user">
+    <h1>This is vue {{ username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["username"],
+};
+</script>
